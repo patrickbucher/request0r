20c672a40eff893d51e7801cb897fbf4476e978a
Patrick Bucher <patrick.bucher@mailbox.org>
2023-12-02 21:36:43 +0100

    additional stats: median, 25th and 75th percentile

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Request URL `https://go.dev/` with `3` workers, performing `5` requests each (15 requests):
 
     $ go run main.go -w 3 -r 5 https://go.dev/
-          mean time   requests     passed     failed
-       820.648671ms         15         15          0
+            mean          25%          50%          75%   requests     passed     failed
+     523.07818ms 503.101025ms 515.632276ms 515.632276ms         15         15          0
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sort"
 	"sync"
 	"time"
 )
@@ -16,6 +17,16 @@ type WorkerResult struct {
 }
 
 type Results map[int][]WorkerResult
+
+type Stats struct {
+	Total  int
+	Passed int
+	Failed int
+	Mean   time.Duration
+	Perc25 time.Duration
+	Perc50 time.Duration
+	Perc75 time.Duration
+}
 
 func main() {
 	workers := flag.Int("w", 1, "number of concurrent workers")
@@ -38,10 +49,12 @@ func main() {
 
 	results := run(flag.Args()[0], *workers, *requests, *state)
 
-	nGood, nBad, duration := stats(results)
+	s := stats(results)
 	nTotal := *workers * *requests
-	fmt.Printf("%15s %10s %10s %10s\n", "mean time", "requests", "passed", "failed")
-	fmt.Printf("%15s %10d %10d %10d\n", duration, nTotal, nGood, nBad)
+	fmt.Printf("%12s %12s %12s %12s %10s %10s %10s\n",
+		"mean", "25%", "50%", "75%", "requests", "passed", "failed")
+	fmt.Printf("%12s %12s %12s %12s %10d %10d %10d\n",
+		s.Mean, s.Perc25, s.Perc50, s.Perc75, nTotal, s.Passed, s.Failed)
 }
 
 func run(url string, workers, requests, okState int) Results {
@@ -96,21 +109,32 @@ func get(url string, okState, workerID int) WorkerResult {
 	return WorkerResult{res.StatusCode == okState, time.Since(start), workerID}
 }
 
-func stats(results Results) (int, int, time.Duration) {
-	var mean, duration time.Duration
+func stats(results Results) Stats {
+	var total time.Duration
+	var stats Stats
+	var durations []time.Duration
 	var ok, nok int
 	for _, rs := range results {
 		for _, r := range rs {
 			if r.OK {
-				duration += r.Time
+				durations = append(durations, r.Time)
+				total += r.Time
 				ok++
 			} else {
 				nok++
 			}
 		}
 	}
+	stats.Total = ok + nok
+	stats.Passed = ok
+	stats.Failed = nok
 	if ok > 0 {
-		mean = duration / time.Duration(ok)
+		stats.Mean = total / time.Duration(ok)
+		sort.Slice(durations, func(l, r int) bool { return l < r })
+		n := len(durations) / 2
+		stats.Perc25 = durations[n/4]
+		stats.Perc50 = durations[n/2]
+		stats.Perc75 = durations[n/4*3]
 	}
-	return ok, nok, mean
+	return stats
 }
